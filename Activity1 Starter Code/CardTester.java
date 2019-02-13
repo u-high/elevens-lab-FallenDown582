4d79131433db3e4d00de5f9aa104661c0d76b0a5
Reggie Adams <reggieadams5802@gmail.com>
2019-02-13 14:55:36 -0600

    Elevens Day 1
    My progress in making the game Elevens after the first day.

--- Activity1 Starter Code/CardTester.java
+++ Activity1 Starter Code/CardTester.java
@@ -3,11 +3,15 @@
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+ /**
+  * The main method in this class checks the Card operations for consistency.
+  * @param args is not used.
+  */
+ public static void main(String[] args) {
+  /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+   Card lancer = new Card("Jack", "Spades", 1);
+   //System.out.println(lancer.pointValue());
+   Card lancerTwo = new Card("Jack", "Spades", 1);
+   System.out.println(lancerTwo.matches(lancer));
+ }
 }
