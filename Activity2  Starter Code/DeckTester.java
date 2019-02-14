c1d2a007f6555d6ee67d88bcd24c9ee9ca93be33
Reggie Adams <reggieadams5802@gmail.com>
2019-02-14 15:00:08 -0600

    Elevens Day Two
    My progress in making the game Elevens after day 2 (Deck Class is working, I think.)

--- Activity2  Starter Code/DeckTester.java
+++ Activity2  Starter Code/DeckTester.java
@@ -3,11 +3,14 @@
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+ /**
+  * The main method in this class checks the Deck operations for consistency.
+  * @param args is not used.
+  */
+ public static void main(String[] args) {
+   String[] suits = {"Clubs", "Diamonds", "Spades", "Hearts"};
+   int[] ptVals = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3};
+   String[] ranks = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+  Deck delta = new Deck(ranks, suits, ptVals);
+ }
 }
